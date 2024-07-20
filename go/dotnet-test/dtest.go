80847e0494fd8d7b16d8af91877ae0f3f5a46bd8
Nick Allen <nick.allen@dataglide.co>
2024-07-20 23:49:37 +0100

    Show dotnet test output with colours and add summary at end

--- go/dotnet-test/dtest.go
+++ go/dotnet-test/dtest.go
@@ -1,21 +1,67 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"log"
+	"io"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	// Define the command and its arguments
-	cmd := exec.Command("dotnet", "test")
+	command := "dotnet test"
 
-	// Run the command and capture the output
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("Command execution failed: %v", err)
+	var stdoutBuf bytes.Buffer
+
+	stdoutMultiWriter := io.MultiWriter(os.Stdout, &stdoutBuf)
+
+	// Use the `script` command to run `dotnet test` in a way that preserves colors
+	scriptCommand := fmt.Sprintf("script -q -c '%s' /dev/null", command)
+	cmd := exec.Command("sh", "-c", scriptCommand)
+
+	cmd.Stdout = stdoutMultiWriter
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error starting Cmd:", err)
+		return
 	}
 
-	// Print the output
-	fmt.Printf("Output: %s\n", string(output))
+	if err := cmd.Wait(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd:", err)
+		return
+	}
+
+	stdoutStr := stdoutBuf.String()
+
+	var passedLines []string
+	var failedLines []string
+	lines := strings.Split(stdoutStr, "\n")
+
+	for _, line := range lines {
+		if strings.Contains(line, "Passed!") && SliceContains(passedLines, line) == false {
+			passedLines = append(passedLines, line)
+		} else if strings.Contains(line, "Failed!") && SliceContains(failedLines, line) == false {
+			failedLines = append(failedLines, line)
+		}
+	}
+
+	for _, passedLine := range passedLines {
+		fmt.Println(passedLine)
+	}
+
+	for _, failedLine := range failedLines {
+		fmt.Println(failedLine)
+	}
+}
+
+func SliceContains(slice []string, value string) bool {
+	for _, s := range slice {
+		if s == value {
+			return true
+		}
+	}
+
+	return false
 }
