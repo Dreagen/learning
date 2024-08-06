e9d12b6b5552de2028bf80d284e71504c2460479
Nick Allen <nick.allen@dataglide.co>
2024-08-06 22:58:50 +0100

    Implement quick sort

--- go/algorithms/quick-sort/quick-sort.go
+++ go/algorithms/quick-sort/quick-sort.go
@@ -7,4 +7,37 @@ func main() {
 }
 
 func QuickSort(arr *[]int) {
+	qs(arr, 0, len(*arr)-1)
+}
+
+func qs(arr *[]int, low int, high int) {
+	if low >= high {
+		return
+	}
+
+	pivotIndex := parition(arr, low, high)
+
+	qs(arr, low, pivotIndex-1)
+	qs(arr, pivotIndex+1, high)
+}
+
+func parition(arr *[]int, low int, high int) int {
+	pivot := (*arr)[high]
+
+	index := low - 1
+
+	for i := low; i < high; i++ {
+		if (*arr)[i] <= pivot {
+			index++
+			tmp := (*arr)[i]
+			(*arr)[i] = (*arr)[index]
+			(*arr)[index] = tmp
+		}
+	}
+
+	index++
+	(*arr)[high] = (*arr)[index]
+	(*arr)[index] = pivot
+
+	return index
 }
