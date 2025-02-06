1102f92cf76cba2c355b4c26a2a08a0ddc377543
Nick Allen <nick.allen@dataglide.co>
2025-02-06 23:07:53 +0000

    Complete traits module of rust book

--- rust/traits/src/main.rs
+++ rust/traits/src/main.rs
@@ -1,72 +1,24 @@
 use std::fmt::Display;
 
-pub struct NewsArticle {
-    pub author: String,
-    pub headline: String,
-    pub content: String,
+struct Pair<T> {
+    x: T,
+    y: T,
 }
 
-impl Summary for NewsArticle {
-    fn summarize_author(&self) -> String {
-        format!("{}", self.author)
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
     }
 }
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
     }
 }
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
-
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-pub fn notify(item1: &impl Summary, item2: &impl Summary) {
-    println!(
-        "Breaking news! {} and {}",
-        item1.summarize(),
-        item2.summarize()
-    );
-}
-
-// pub fn notify<T>(item1: &T, item2: &T)
-// where
-//     T: Summary + Display,
-// {
-// }
-
-fn main() {
-    let tweet = Tweet {
-        username: String::from("@johndoe"),
-        content: String::from("Hello twittersphere!"),
-        reply: false,
-        retweet: false,
-    };
-
-    let news_article = NewsArticle {
-        author: String::from("John Doe"),
-        headline: String::from("The Sky is Falling!"),
-        content: String::from("The sky is not actually falling."),
-    };
-
-    println!("Tweet summary: {}", tweet.summarize());
-    println!("News Article summary: {}", news_article.summarize());
-
-    notify(&news_article, &tweet);
-}
+fn main() {}
