f8cf377904cb83ba0e369537073646c2f148951e
Nick Allen <nick.allen@dataglide.co>
2025-02-07 23:06:46 +0000

    Complete lifetimes chapter of rust book

--- rust/lifetimes/src/main.rs
+++ rust/lifetimes/src/main.rs
@@ -1,12 +1,31 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = String::from("xyz");
+use std::fmt::Display;
 
-    let result = longest(string1.as_str(), string2.as_str());
-    println!("The longest string is {}", result);
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+impl<'a> ImportantExcerpt<'a> {
+    fn return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael, Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("{}", i.return_part("some announcement"));
+}
+
+fn longest_with_an_annoucement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement: {}", ann);
     if x.len() > y.len() {
         x
     } else {
