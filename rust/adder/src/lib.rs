8deedc8404184f5154459eb2ee65eca715301148
Nick Allen <nick.allen@dataglide.co>
2025-03-12 23:39:09 +0000

    Finish part 1 of testing module of rust book

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -1,3 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Guess {
+    value: i32,
 }
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}",
+                value
+            );
+        }
+
+        if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
--- rust/adder/tests/tests.rs
+++ rust/adder/tests/tests.rs
@@ -1,15 +1,13 @@
 #[cfg(test)]
 mod tests {
-    use adder::add;
+    use adder::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn failing_test() {
-        panic!("make this test fail");
+    fn it_works() -> Result<(), String> {
+        if 2 + 3 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
