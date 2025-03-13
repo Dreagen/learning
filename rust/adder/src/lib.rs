7c2abc30f81c3842bfa2b6b593b57f05e0199e6f
Nick Allen <nick.allen@dataglide.co>
2025-03-13 22:38:14 +0000

    Complete testing part 2 of rust book series

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -1,24 +1,17 @@
-pub struct Guess {
-    value: i32,
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
 }
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}",
-                value
-            );
-        }
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
 
-        if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}",
-                value
-            );
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        Guess { value }
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
-
