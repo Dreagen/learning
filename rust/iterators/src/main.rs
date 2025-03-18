3ec028c4645c36b8b79acdbbaa5179cabe5f83b2
Nick Allen <nick.allen@dataglide.co>
2025-03-18 23:18:48 +0000

    Start iterators chapter of rust book

---
+++ rust/iterators/src/main.rs
@@ -0,0 +1,47 @@
+pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
+
+#[test]
+fn iterator_map() {
+    let v1 = vec![1, 2, 3];
+
+    let mapped: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(mapped, vec![2, 3, 4]);
+}
+
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for value in v1_iter {
+        println!("Got {}", value)
+    }
+}
