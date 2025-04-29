72975c5be04e01dd10800b9a1621002d7075db5d
Nick Allen <nick.allen@dataglide.co>
2025-04-29 14:58:03 +0100

    Finish patterns and matching part of rust book

--- rust/patterns/src/main.rs
+++ rust/patterns/src/main.rs
@@ -34,5 +34,52 @@ pub fn if_let() {
         } else {
             println!("Authorization status: basic");
         }
+    } else {
+        println!("Authorization status: guest");
+    }
+}
+
+pub fn while_let() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+pub fn for_loop() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
+
+pub fn let_statement() {
+    let x = 5;
+    let (x, y, z) = (1, 2, 3);
+}
+
+pub fn function_params() {
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+fn irrefutable_refutable() {
+    // irrefutable - x will always match whatever is on the right
+    let x = 5;
+
+    // refutable - Some(x) will only match if x is the Some variant of the option
+    let x: Option<i32> = None;
+    if let Some(x) = x {
+        println!("{}", x);
     }
 }
