4a96b936cb5ea90b6a50f8d60be539ee48973f7c
Nick Allen <nick.allen@dataglide.co>
2025-05-05 23:33:01 +0100

    Complete unsafe part of rust book

--- rust/unsafe_rust/src/main.rs
+++ rust/unsafe_rust/src/main.rs
@@ -1,11 +1,24 @@
-fn main() {
-    let mut num = 5;
+use std::slice;
 
-    let r1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+fn main() {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let (a, b) = &v.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+pub fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
 
     unsafe {
-        println!("r1 is: {}", *r1);
-        println!("r2 is: {}", *r2);
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
     }
 }
