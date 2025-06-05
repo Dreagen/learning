2a77102f4afba32f395ee8d601c496c00b11f6f9
Nick Allen <nick.allen@dataglide.co>
2025-06-05 18:02:50 +0100

    Continue rust web server chatper of rustbook

--- rust/web-server/src/main.rs
+++ rust/web-server/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,9 +19,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buf).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buf.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
+    } else if buf.starts_with(sleep) {
+        std::thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "sleep.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
