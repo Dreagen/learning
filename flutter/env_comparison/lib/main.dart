24b096dbb73b310a812fd0647e8647b6491bcae5
Nick Allen <nick.allen@dataglide.co>
2025-06-12 00:17:19 +0100

    Add a repo and some fake data to start messing with flutter ui

--- flutter/env_comparison/lib/main.dart
+++ flutter/env_comparison/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'repository.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -87,7 +89,31 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final Repository repository = Repository();
+  List<ComparisonSummary> comparisonData = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
+  Future<void> _loadData() async {
+    final data = await repository.getItems();
+
+    setState(() {
+      comparisonData = data;
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -96,12 +122,18 @@ class MainPage extends StatelessWidget {
         final double horizontalPadding = constraints.maxWidth > 800
             ? 50.0
             : 20.0;
-        final double verticalPadding = constraints.maxHeight > 600
-            ? 50.0
-            : 20.0;
+        final double verticalPadding = 20;
 
         // Calculate cards per row based on available space
-        final int crossAxisCount = constraints.maxWidth > 600 ? 1 : 1;
+        final int crossAxisCount = constraints.maxWidth > 1500 ? 2 : 1;
+
+        if (isLoading) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (comparisonData.isEmpty) {
+          return Center(child: Text("No comparison data available"));
+        }
 
         return Padding(
           padding: EdgeInsets.only(
@@ -112,26 +144,144 @@ class MainPage extends StatelessWidget {
           ),
           child: GridView.count(
             crossAxisCount: crossAxisCount,
-            childAspectRatio: 2.5,
+            childAspectRatio: 2,
             mainAxisSpacing: 10,
             crossAxisSpacing: 10,
             children: [
               Card(
                 child: Padding(
                   padding: const EdgeInsets.all(20),
-                  child: Text("Card 1"),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "BASE",
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 15),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth:
+                              constraints.maxWidth -
+                              60, // Card padding (20) × 2 + some buffer
+                        ),
+                        child: DataTable(
+                          columnSpacing: 24.0,
+                          horizontalMargin: 12.0,
+                          columns: const [
+                            DataColumn(label: Text('ISIN')),
+                            DataColumn(label: Text('Currency')),
+                            DataColumn(label: Text('Status')),
+                            DataColumn(label: Text('Last Change')),
+                            DataColumn(label: Text('Processed')),
+                          ],
+                          rows: comparisonData[0].topicComparisonSummary.base
+                              .map(
+                                (item) => DataRow(
+                                  cells: [
+                                    DataCell(Text(item.isinCode)),
+                                    DataCell(Text(item.currency)),
+                                    DataCell(Text(item.status)),
+                                    DataCell(Text(item.lastChangeType)),
+                                    DataCell(Text(item.lastChangeProcessed)),
+                                  ],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Card(
                 child: Padding(
                   padding: const EdgeInsets.all(20),
-                  child: Text("Card 2"),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "COMPARISON",
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 15),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth:
+                              constraints.maxWidth -
+                              60, // Card padding (20) × 2 + some buffer
+                        ),
+                        child: DataTable(
+                          columnSpacing: 24.0,
+                          horizontalMargin: 12.0,
+                          columns: const [
+                            DataColumn(label: Text('ISIN')),
+                            DataColumn(label: Text('Currency')),
+                            DataColumn(label: Text('Status')),
+                            DataColumn(label: Text('Last Change')),
+                            DataColumn(label: Text('Processed')),
+                          ],
+                          rows: comparisonData[0]
+                              .topicComparisonSummary
+                              .comparison
+                              .map(
+                                (item) => DataRow(
+                                  cells: [
+                                    DataCell(Text(item.isinCode)),
+                                    DataCell(Text(item.currency)),
+                                    DataCell(Text(item.status)),
+                                    DataCell(Text(item.lastChangeType)),
+                                    DataCell(Text(item.lastChangeProcessed)),
+                                  ],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Card(
                 child: Padding(
                   padding: const EdgeInsets.all(20),
-                  child: Text("Card 3"),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "RESULTS",
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 15),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth:
+                              constraints.maxWidth -
+                              60, // Card padding (20) × 2 + some buffer
+                        ),
+                        child: DataTable(
+                          columns: const [
+                            DataColumn(label: Text('Action')),
+                            DataColumn(label: Text('ISIN')),
+                            DataColumn(label: Text('Currency')),
+                            DataColumn(label: Text('Change')),
+                          ],
+                          rows: comparisonData[0].topicComparisonSummary.result
+                              .map(
+                                (item) => DataRow(
+                                  cells: [
+                                    DataCell(Text(item.action)),
+                                    DataCell(Text(item.isinCode)),
+                                    DataCell(Text(item.currency)),
+                                    DataCell(Text(item.lastChangeProcessed)),
+                                  ],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
---
+++ flutter/env_comparison/lib/repository.dart
@@ -0,0 +1,200 @@
+class Repository {
+  Future<List<ComparisonSummary>> getItems() async {
+    var topicData = createFakeTopicComparison();
+    var shareClassData = createFakeShareClassData();
+
+    return [
+      (
+        topicComparisonSummary: topicData,
+        shareClassComparisonSummary: shareClassData,
+      ),
+    ];
+  }
+
+  TopicComparisonSummary createFakeTopicComparison() {
+    final base1 = (
+      isinCode: 'LU0808551500',
+      currency: 'EUR',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"EUR"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'true',
+    );
+
+    final base2 = (
+      isinCode: 'LU1227550453',
+      currency: 'EUR',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"EUR"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'true',
+    );
+
+    final base3 = (
+      isinCode: 'LU2407564710',
+      currency: 'USD',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"USD"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'true',
+    );
+
+    final comparison1 = (
+      isinCode: 'LU0808551500',
+      currency: 'EUR',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"EUR"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'false',
+    );
+
+    final comparison2 = (
+      isinCode: 'LU1227550453',
+      currency: 'EUR',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"EUR"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'false',
+    );
+
+    final comparison3 = (
+      isinCode: 'LU2407564710',
+      currency: 'USD',
+      clientId: 'FECC23FA-5D41-4156-A170-91D7A6F3C7BB',
+      mappedJson: '{"pubcry":"USD"}',
+      status: 'Active',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'false',
+    );
+
+    final comparisonResult1 = (
+      action: 'MOD',
+      isinCode: 'LU1227550453',
+      currency: 'EUR',
+      mappedJson: '{"pubcry":"EUR"}',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'DIFF(true->false)',
+    );
+
+    final comparisonResult2 = (
+      action: 'MOD',
+      isinCode: 'LU2407564710',
+      currency: 'USD',
+      mappedJson: '{"pubcry":"USD"}',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'DIFF(true->false)',
+    );
+
+    final comparisonResult3 = (
+      action: 'MOD',
+      isinCode: 'LU0808551500',
+      currency: 'EUR',
+      mappedJson: '{"pubcry":"EUR"}',
+      lastChangeType: 'ADD',
+      lastChangeProcessed: 'DIFF(true->false)',
+    );
+
+    return (
+      dataType: "topic",
+      base: [base1, base2, base3],
+      comparison: [comparison1, comparison2, comparison3],
+      result: [comparisonResult1, comparisonResult2, comparisonResult3],
+    );
+  }
+
+  ShareClassComparisonSummary createFakeShareClassData() {
+    final base1 = (
+      isinCode: 'LU0808551500',
+      mappedJson:
+          '{"cod_isin":"LU0808551500","inst_scrnam":"Class EUR Shares","inst_scrsts":"Dormant","scrcry":"EUR"}',
+    );
+
+    final base2 = (
+      isinCode: 'LU1227550453',
+      mappedJson:
+          '{"cod_isin":"LU1227550453","inst_scrnam":"Ordinary Shares","inst_scrsts":"Dormant","scrcry":"EUR"}',
+    );
+
+    final base3 = (
+      isinCode: 'LU2407564710',
+      mappedJson:
+          '{"cod_isin":"LU2407564710","inst_scrnam":"Class A","inst_scrsts":"Dormant","scrcry":"USD"}',
+    );
+
+    final comparison1 = (
+      isinCode: 'LU0808551500',
+      mappedJson:
+          '{"cod_isin":"LU0808551500","inst_scrnam":"Class EUR Shares","inst_scrsts":"Dormant","scrcry":"EUR"}',
+    );
+
+    final comparison2 = (
+      isinCode: 'LU1227550453',
+      mappedJson:
+          '{"cod_isin":"LU1227550453","inst_scrnam":"Ordinary Shares","inst_scrsts":"Dormant","scrcry":"EUR"}',
+    );
+
+    final comparison3 = (
+      isinCode: 'LU2407564710',
+      mappedJson:
+          '{"cod_isin":"LU2407564710","inst_scrnam":"Class A","inst_scrsts":"Dormant","scrcry":"USD"}',
+    );
+
+    return (
+      dataType: "share class",
+      base: [base1, base2, base3],
+      comparison: [comparison1, comparison2, comparison3],
+      result: [], // Empty list as requested
+    );
+  }
+}
+
+typedef ComparisonSummary = ({
+  TopicComparisonSummary topicComparisonSummary,
+  ShareClassComparisonSummary shareClassComparisonSummary,
+});
+
+typedef TopicComparisonSummary = ({
+  String dataType,
+  List<TopicComparison> base,
+  List<TopicComparison> comparison,
+  List<TopicComparisonResult> result,
+});
+
+typedef TopicComparison = ({
+  String isinCode,
+  String currency,
+  String clientId,
+  String mappedJson,
+  String status,
+  String lastChangeType,
+  String lastChangeProcessed,
+});
+
+typedef TopicComparisonResult = ({
+  String action,
+  String isinCode,
+  String currency,
+  String mappedJson,
+  String lastChangeType,
+  String lastChangeProcessed,
+});
+
+typedef ShareClassComparisonSummary = ({
+  String dataType,
+  List<ShareClassComparison> base,
+  List<ShareClassComparison> comparison,
+  List<ShareClassComparisonResult> result,
+});
+
+typedef ShareClassComparison = ({String isinCode, String mappedJson});
+
+typedef ShareClassComparisonResult = ({
+  String action,
+  String isinCode,
+  String mappedJson,
+});
