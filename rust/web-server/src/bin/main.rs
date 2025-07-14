c6cd4fc215633ce1f6a08071c54f905f09080877
Nick Allen <nick.allen@dataglide.co>
2025-07-14 23:01:46 +0100

    Allow setting a port a sleep time for requests via cli arguments

--- rust/web-server/src/bin/main.rs
+++ rust/web-server/src/bin/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,34 +8,44 @@ use std::time::Duration;
 use web_server::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    let port = args.get(1);
+    let default_port = &"7878".to_string();
+    let port = port.unwrap_or(&default_port);
+
+    let mut delay: Option<u64> = None;
+    if let Some(d) = args.get(2) {
+        delay = Some(d.parse().expect("Delay must be a valid integer"));
+    }
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
 
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        pool.execute(|| {
-            handle_connection(stream);
+        pool.execute(move || {
+            handle_connection(stream, delay);
         });
     }
 
     println!("Shutting down.");
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, delay: Option<u64>) {
     let mut buf = [0; 1024];
 
     stream.read(&mut buf).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buf.starts_with(get) {
+        if let Some(sleep_for) = delay {
+            std::thread::sleep(Duration::from_millis(sleep_for));
+        }
         ("HTTP/1.1 200 OK", "index.html")
-    } else if buf.starts_with(sleep) {
-        std::thread::sleep(Duration::from_secs(15));
-        ("HTTP/1.1 200 OK", "sleep.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
