d7389c5f6dbb8fe0ee7d17853635184570e11005
Nick Allen <nick.allen@dataglide.co>
2025-07-16 00:07:36 +0100

    Make load balancer handle servers going down

--- rust/load-balancer/src/lib.rs
+++ rust/load-balancer/src/lib.rs
@@ -3,7 +3,12 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+const GREEN: &str = "\x1b[32m";
+const RED: &str = "\x1b[31m";
+const RESET: &str = "\x1b[0m";
+
 pub fn start(servers: Vec<String>) {
+    println!("Load balancing between servers:");
     for server in &servers {
         println!("   {}", server);
     }
@@ -16,39 +21,52 @@ pub fn start(servers: Vec<String>) {
         let tcp_stream = stream.unwrap();
 
         let server_number = current_server % server_count;
-        println!("Forwarding request to server {server_number}");
 
-        let handle_result = handle_connection(tcp_stream, &servers[server_number]);
+        let (mut tcp_stream, buf) = read_stream(tcp_stream);
+        for i in 0..server_count {
+            let server_number = (server_number + i) % server_count;
+            print!("Forwarding request to server {server_number}");
 
-        // TODO: don't drop request if it fails, try all other servers
-        if handle_result.is_err() {
-            println!(
-                "Failed to connecting to server {} with error: {}",
-                server_number,
-                handle_result.unwrap_err()
-            );
+            let result_data = forward_to_server(&buf, &servers[server_number]);
+            if let Ok(result) = result_data {
+                println!("{} -> Success! {}", GREEN, RESET);
+                tcp_stream.write(&result).unwrap();
+                break;
+            }
+
+            println!("{} -> Failed! - {}{}", RED, result_data.unwrap_err(), RESET);
+
+            if i == server_count - 1 {
+                println!(
+                    "{}\nAll servers failed to respond, request not handled\n{}",
+                    RED, RESET
+                );
+            }
         }
 
         current_server += 1;
     }
 }
 
-fn handle_connection(
-    mut incoming_stream: TcpStream,
-    server: &String,
-) -> Result<(), std::io::Error> {
+fn read_stream(mut stream: TcpStream) -> (TcpStream, Vec<u8>) {
     let mut buf = [0; 1024];
 
-    let amount_read = incoming_stream.read(&mut buf).unwrap();
+    let amount_read = stream.read(&mut buf).unwrap();
+
+    (stream, buf[..amount_read].to_vec())
+}
+
+fn forward_to_server(incoming_data: &Vec<u8>, server: &String) -> Result<Vec<u8>, std::io::Error> {
+    let mut buf = [0; 1024];
+
     let tcp_stream_result = TcpStream::connect(server);
 
     match tcp_stream_result {
         Ok(mut tcp_stream) => {
-            tcp_stream.write(&buf[..amount_read]).unwrap();
+            tcp_stream.write(&incoming_data).unwrap();
             let amount_read = tcp_stream.read(&mut buf).unwrap();
 
-            incoming_stream.write(&buf[..amount_read]).unwrap();
-            return Ok(());
+            return Ok(buf[..amount_read].to_vec());
         }
         Err(e) => {
             return Err(e);
