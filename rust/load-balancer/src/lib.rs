799d3ee557ae54bdb954500e89e0041b4d191f8f
Nick Allen <nick.allen@dataglide.co>
2025-07-15 18:03:12 +0100

    Start handling when a server is down

--- rust/load-balancer/src/lib.rs
+++ rust/load-balancer/src/lib.rs
@@ -18,20 +18,40 @@ pub fn start(servers: Vec<String>) {
         let server_number = current_server % server_count;
         println!("Forwarding request to server {server_number}");
 
-        handle_connection(tcp_stream, &servers[server_number]);
+        let handle_result = handle_connection(tcp_stream, &servers[server_number]);
+
+        // TODO: don't drop request if it fails, try all other servers
+        if handle_result.is_err() {
+            println!(
+                "Failed to connecting to server {} with error: {}",
+                server_number,
+                handle_result.unwrap_err()
+            );
+        }
+
         current_server += 1;
     }
 }
 
-fn handle_connection(mut incoming_stream: TcpStream, server: &String) {
+fn handle_connection(
+    mut incoming_stream: TcpStream,
+    server: &String,
+) -> Result<(), std::io::Error> {
     let mut buf = [0; 1024];
 
-    incoming_stream.read(&mut buf).unwrap();
+    let amount_read = incoming_stream.read(&mut buf).unwrap();
+    let tcp_stream_result = TcpStream::connect(server);
 
-    let mut tcp_stream = TcpStream::connect(server).unwrap();
+    match tcp_stream_result {
+        Ok(mut tcp_stream) => {
+            tcp_stream.write(&buf[..amount_read]).unwrap();
+            let amount_read = tcp_stream.read(&mut buf).unwrap();
 
-    tcp_stream.write(&buf).unwrap();
-    tcp_stream.read(&mut buf).unwrap();
-
-    incoming_stream.write(&buf).unwrap();
+            incoming_stream.write(&buf[..amount_read]).unwrap();
+            return Ok(());
+        }
+        Err(e) => {
+            return Err(e);
+        }
+    }
 }
